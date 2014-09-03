b717ceccf9245b7e2da6dc26cb1c42e39e3b7920
Brandon Burton <brandon@inatree.org>
2014-09-03 11:21:01 -0700

    workaround for apt/build-essential issue

--- recipes/default.rb
+++ recipes/default.rb
@@ -13,5 +13,9 @@ node.set["apt"]["compile_time_update"] = true
 node.set["apt"]["periodic_update_min_delay"] = 0
 include_recipe "apt::default"
 
+execute 'apt-get update' do
+  action :nothing
+end.run_action(:run)
+
 # let's install zookeeper
 include_recipe 'zookeeper::install'
